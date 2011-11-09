4242af8e02ed838753c93dfb425e0657ec6a626c
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-11-09 00:14:31 +0000

    seed Continent and Country tables with XML data from cpp-web

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed Continents and Countries with xml data
+require 'open-uri'
+
+h = Hash.from_xml(open('https://raw.github.com/ChokePointProject/cpp-web/master/tmp/countryInfo.xml'))['geonames']['country']
+
+# 1st sweep: continents
+h.each do |c|
+  Continent.find_or_create_by_code_and_name(c['continent'], c['continentName'])
+end
+
+# 2nd sweep: countries
+h.each do |c|
+  Country.create(
+    :continent_id => Continent.find_by_code(c['continent']),
+    :code => c['countryCode'],
+    :name => c['countryName'],
+    :isoNumeric => c['isoNumeric'],
+    :isoAlpha3 => c['isoAlpha3'],
+    :fipsCode => c['fipsCode'],
+    :capital => c['capital'],
+    :areaInSqKm => c['areaInSqKm'],
+    :population => c['population'],
+    :currencyCode => c['currencyCode'],
+    :languages => c['languages'],
+    :geonameId => c['geonameId'],
+    :bBoxWest => c['bBoxWest'],
+    :bBoxNorth => c['bBoxNorth'],
+    :bBoxEast => c['bBoxEast'],
+    :bBoxSouth => c['bBoxSouth']
+  )
+end
+
